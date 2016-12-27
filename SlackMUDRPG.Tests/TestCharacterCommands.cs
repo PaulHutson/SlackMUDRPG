ae49e8e60b0364892c7b4482104fa1348e31620a
PaulHutson <hutsonphutty+github@gmail.com>
2016-12-27 17:05:14 +0000

    Added tests; they don't work.. yet. Also removed the application insights.

---
+++ SlackMUDRPG.Tests/TestCharacterCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SlackMUDRPG.Tests
+{
+    [TestClass]
+    public class TestCharacterCommands
+    {
+        [TestMethod]
+        public void Test_Login_NoChar()
+        {
+            string a = CommandsClasses.SlackMud.Login("THISWILLFAIL");
+            string  expected = "You must create a character, to do so, use the command /sm CreateCharacter FIRSTNAME,LASTNAME,SEX,AGE\n";
+                    expected += "i.e. /sm CreateCharacter Paul,Hutson,m,34";
+            Assert.AreEqual(a, expected, false);
+        }
+
+        [TestMethod]
+        public void Test_Login_Char()
+        {
+            string a = CommandsClasses.SlackMud.Login("123");
+            string expected = "You must create a character, to do so, use the command /sm CreateCharacter FIRSTNAME,LASTNAME,SEX,AGE\n";
+            expected += "i.e. /sm CreateCharacter Paul,Hutson,m,34";
+            Assert.AreEqual(a, expected, false);
+        }
+    }
+}
