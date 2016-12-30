a78bb8dec72b237bd1343e45d99057298edd9b9b
Paul <Paul@PAUL-GAMING-PC>
2016-12-30 15:42:08 +0000

    Updated the rooms functionality, preparing move.

--- SlackMUDRPG/CommandClasses/SMCharacter.cs
+++ SlackMUDRPG/CommandClasses/SMCharacter.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using SlackMUDRPG.CommandClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,9 +36,12 @@ namespace SlackMUDRPG.CommandsClasses
         [JsonProperty("RoomLocation")]
         public string RoomLocation { get; set; }
 
+        [JsonProperty("Attributes")]
+        public SMAttributes Attributes { get; set; }
+
         [JsonProperty("CharacterItems")]
         public List<SMItem> CharacterItems { get; set; }
-
+        
         [JsonProperty("Skills")]
         public List<SMSkill> Skills { get; set; }
 
@@ -54,5 +58,65 @@ namespace SlackMUDRPG.CommandsClasses
 
 			this.CharacterItems.Add(item);
 		}
+
+        /// <summary>
+        /// Move the character.
+        /// </summary>
+        /// <param name="charID"></param>
+        /// <param name="exitShortcut"></param>
+        /// <returns></returns>
+        public string Move(string exitShortcut)
+        {
+            // Create returnString
+            string returnString = "";
+
+            // Get the current character location
+            List<SMCharacter> smcs = new List<SMCharacter>();
+            smcs = (List<SlackMUDRPG.CommandsClasses.SMCharacter>)HttpContext.Current.Application["SMCharacters"];
+            SMCharacter charToMove;
+            bool foundCharacter = false;
+
+            // Check if there are characters actually on the server to move...
+            if (smcs != null)
+            {
+                if (smcs.FirstOrDefault(smc => smc.UserID == this.UserID) != null)
+                {
+                    // Get the id from the object
+                    charToMove = smcs.FirstOrDefault(smc => smc.UserID == this.UserID);
+                    foundCharacter = true;
+                }
+                else
+                {
+                    returnString = "Character not logged in, please login before trying to move.";
+                }
+            }
+            else
+            {
+                returnString = "Character not logged in, please login before trying to move.";
+            }
+
+            if (foundCharacter)
+            {
+                // Get the exits from that location
+                // TODO get room exits from memory
+
+                // Get the specific exit from the location referred to by the shortcut
+                // TODO check the exits include one for the location
+
+                // Check new room is loaded
+                // TODO Implement
+
+                // Move the player to the new location
+                // TODO change the player location
+
+                // Announce arrival to other players in the same place
+                // Get all players in the location
+
+                // Send a message to those players
+                // TODO 
+            }
+
+            return returnString;
+        }
     }
 }
