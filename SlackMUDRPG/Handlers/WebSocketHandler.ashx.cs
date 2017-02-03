7a568b8f3a0d10d4d2cfba4de800d5c14b82730e
Paul <Paul@PAUL-GAMING-PC>
2017-02-03 19:39:17 +0000

    Basis for the websocket implementation (not working! :()

--- SlackMUDRPG/Handlers/WebSocketHandler.ashx.cs
+++ SlackMUDRPG/Handlers/WebSocketHandler.ashx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Web;
-using System.Web.WebSockets;
 
 namespace SlackMUDRPG.Handlers
 {
@@ -16,58 +13,17 @@ namespace SlackMUDRPG.Handlers
 
 		public void ProcessRequest(HttpContext context)
 		{
-			//Checks if the query is WebSocket request.  
 			if (context.IsWebSocketRequest)
 			{
-				//If yes, we attach the asynchronous handler. 
-				context.AcceptWebSocketRequest(WebSocketRequestHandler);
+				context.AcceptWebSocketRequest(new TestWebSocketHandler());
 			}
 		}
 
-		public bool IsReusable { get { return false; } }
-
-		//Asynchronous request handler. 
-		public async Task WebSocketRequestHandler(AspNetWebSocketContext webSocketContext)
+		public bool IsReusable
 		{
-			//Gets the current WebSocket object. 
-			WebSocket webSocket = webSocketContext.WebSocket;
-
-			/* We define a certain constant which will represent 
-            size of received data. It is established by us and  
-            we can set any value. We know that in this case the size of the sent 
-            data is very small. */
-			const int maxMessageSize = 1024;
-
-			//Buffer for received bits. 
-			var receivedDataBuffer = new ArraySegment<Byte>(new Byte[maxMessageSize]);
-
-			var cancellationToken = new CancellationToken();
-
-			//Checks WebSocket state. 
-			while (webSocket.State == WebSocketState.Open)
+			get
 			{
-				//Reads data. 
-				WebSocketReceiveResult webSocketReceiveResult = await webSocket.ReceiveAsync(receivedDataBuffer, cancellationToken);
-
-				//If input frame is cancelation frame, send close command. 
-				if (webSocketReceiveResult.MessageType == WebSocketMessageType.Close)
-				{
-					await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, String.Empty, cancellationToken);
-				}
-				else
-				{
-					byte[] payloadData = receivedDataBuffer.Array.Where(b => b != 0).ToArray();
-
-					//Because we know that is a string, we convert it. 
-					string receiveString = System.Text.Encoding.UTF8.GetString(payloadData, 0, payloadData.Length);
-
-					//Converts string to byte array. 
-					var newString = String.Format("Hello, " + receiveString + " ! Time {0}", DateTime.Now.ToString());
-					Byte[] bytes = System.Text.Encoding.UTF8.GetBytes(newString);
-
-					//Sends data back. 
-					await webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, cancellationToken);
-				}
+				return false;
 			}
 		}
 	}
