a496328b6ac29fc2785a1ca0c6d04376ad534729
Robert Curran <robert.d.curran@googlemail.com>
2017-02-05 16:15:24 +0000

    Rename SPCommandUtility to SMCommandUtility and move to its own file. Adds SMCommandHelper to delagate processing of user command inputs. Removes PassQueryParamAsFirstArg and renames CommandExpression to ParamsExpression on SMCommand class. Update SMCommand::ParseExpression to handle comma seperated lists for CommandName to facilitate command aliases.

--- SlackMUDRPG/CommandClasses/SMCommand.cs
+++ SlackMUDRPG/CommandClasses/SMCommand.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System.Web;
-using SlackMUDRPG.Utility;
-using System.Threading;
 
 namespace SlackMUDRPG.CommandClasses
 {
@@ -26,8 +24,8 @@ namespace SlackMUDRPG.CommandClasses
 		[JsonProperty("CommandSyntax")]
 		public string CommandSyntax { get; set; }
 
-		[JsonProperty("CommandExpression")]
-		public string CommandExpression { get; set; }
+		[JsonProperty("ParamsExpression")]
+		public string ParamsExpression { get; set; }
 
 		[JsonProperty("CommandClass")]
 		public string CommandClass { get; set; }
@@ -38,9 +36,6 @@ namespace SlackMUDRPG.CommandClasses
 		[JsonProperty("PassCommandAsFirstArg")]
 		public bool PassCommandAsFirstArg { get; set; }
 
-		[JsonProperty("PassQueryParamAsFirstArg")]
-		public string PassQueryParamAsFirstArg { get; set; }
-
 		[JsonProperty("ExampleUsage")]
 		public string ExampleUsage { get; set; }
 
@@ -48,24 +43,27 @@ namespace SlackMUDRPG.CommandClasses
 		public string RequiredSkill { get; set; }
 
 		/// <summary>
-		/// Parses the CommandExpression into a Regex pattern to extra params from the users command, e.g.
-		/// equip {.+} from {.+}? becomes ^equip (?:equip (.+?)(?: from |$)(.+)?)$
+		/// Parses the ParamsExpression into a Regex pattern to extra params from the users command accounting for aliases, e.g.
+		/// Command Name = equip; "{.+} from {.+}?" becomes "^(?:equip )(?:(.+?)(?: from |$)(.+)?)$"
+		/// Command Name = attack; "{.+}" becomes "^(?:attack |hit )(?:(.+?))$"
 		/// </summary>
 		/// <returns>The parsed regex patthern string</returns>
 		public string ParseExpression()
 		{
 			string pattern = @"(?:\{([^}]+)\}(\?)?)(?:$|)";
 
-			MatchCollection matches = Regex.Matches(this.CommandExpression, pattern);
+			string commandExpression = $"{this.CommandName.Split(',')[0]} {this.ParamsExpression}";
+
+			MatchCollection matches = Regex.Matches(commandExpression, pattern);
 
 			string ret = @"";
 
 			for (int i = 0; i < matches.Count; i++)
 			{
-				// handles adding the line start, command and opens a non-capturing group
+				// handles adding the line start, command (inc aliases) and opens a non-capturing group
 				if (i == 0)
 				{
-					ret += "^" + this.CommandExpression.Substring(0, matches[i].Index);
+					ret += "^(?:" + String.Join(" |", this.CommandName.Split(',').Select(s => s.Trim())) + " )";
 					ret += "(?:";
 				}
 
@@ -104,7 +102,7 @@ namespace SlackMUDRPG.CommandClasses
 
 					int start = current.Index + current.Length;
 					int length = next.Index - (current.Index + current.Length);
-					ret += "(?:" + this.CommandExpression.Substring(start, length);
+					ret += "(?:" + commandExpression.Substring(start, length);
 
 					// if next group is optional
 					if (next.Groups[2].Value == "?")
@@ -123,209 +121,6 @@ namespace SlackMUDRPG.CommandClasses
 			}
 
 			return ret;
-		}
-	}
-
-	public class SPCommandUtility
-	{
-		public string UserID { get; set; }
-
-		public SPCommandUtility(string userID)
-		{
-			this.UserID = userID;
-		}
-
-		public string InitateCommand(string commandText)
-		{
-			// Return string information
-			string returnString = "";
-
-			// TODO Validate user command before threading the command
-			if (this.GetCommandByName(this.GetCommandNameFromString(commandText)) != null)
-			{
-				HttpContext ctx = HttpContext.Current;
-
-				Thread commandThread = new Thread(new ThreadStart(() =>
-				{
-					HttpContext.Current = ctx;
-					this.ProcessUserCommand(commandText);
-				}));
-
-				commandThread.Start();
-			}
-			else
-			{
-				SMCharacter smc = new SlackMud().GetCharacter(this.UserID);
-				returnString = "Command \"" + commandText + "\" not recognised, please check and try again";
-			}
-
-			return returnString;
-		}
-
-		/// <summary>
-		/// Processes the user command, running the action if the command is vaid.
-		/// </summary>
-		/// <param name="cmd">User entered command string.</param>
-		public void ProcessUserCommand(string cmd)
-		{
-			SMParsedCommand parsedCmd = ParseCommandString(cmd);
-
-			if (parsedCmd != null)
-			{
-				RunAction(parsedCmd);
-			}
-		}
-
-		/// <summary>
-		/// Parses the user enterd command string to produced a SMParsedCommand object to run.
-		/// </summary>
-		/// <returns>SMParsedCommand object to run.</returns>
-		/// <param name="cmdString">User entered command string.</param>
-		/// <thorws>NullReferenceException if command not found.
-		public SMParsedCommand ParseCommandString(string cmdString)
-		{
-			if (cmdString == null)
-			{
-				return null;
-			}
-
-			SMParsedCommand cmd = new SMParsedCommand();
-
-			// Trim white space and remove leading /
-			char[] trimFromStart = { '/' };
-			cmdString = Utils.CleanString(cmdString, trimFromStart);
-
-			// Get the and of the command e.g. "shout"
-			string commandName = GetCommandNameFromString(cmdString);
-
-			// Get the SMCommmand object for the given command name
-			SMCommand command = GetCommandByName(commandName);
-
-			// If the command if found extra the params and return the parsed command object.
-			if (command != null)
-			{
-				object[] parameters = GetParamsFromCommandString(command, cmdString);
-
-				cmd.CommandName = commandName;
-				cmd.Command = command;
-				cmd.Parameters = parameters;
-
-				return cmd;
-			}
-
-			// Throw and exception if unable to find the command
-			throw new System.NullReferenceException("Command specified command not found.");
-		}
-
-		/// <summary>
-		/// Gets the command name from the user command string.
-		/// </summary>
-		/// <returns>The command name..</returns>
-		/// <param name="cmdString">User entered command string..</param>
-		public string GetCommandNameFromString(string cmdString)
-		{
-			int spacePos = cmdString.IndexOf(" ", StringComparison.CurrentCulture);
-
-			if (spacePos < 0)
-			{
-				spacePos = cmdString.Length;
-			}
-
-			return cmdString.Substring(0, spacePos);
-		}
-
-		/// <summary>
-		/// Gets an SMCommand object for the command by name.
-		/// </summary>
-		/// <returns>SMCommand object or null</returns>
-		/// <param name="name">Name of the command.</param>
-		public SMCommand GetCommandByName(string name)
-		{
-			List<SMCommand> commands = (List<SMCommand>)HttpContext.Current.Application["SMCommands"];
-
-			SMCommand command = commands.FirstOrDefault(cmd => cmd.CommandName == name);
-
-			return command;
-		}
-
-		/// <summary>
-		/// Gets the parameters from command string based on the expression in the corresponding SMCommand object
-		/// </summary>
-		/// <returns>List of extrated parameters to use when running the user command</returns>
-		/// <param name="command">SMCommand object.</param>
-		/// <param name="cmdString">User entered command string.</param>
-		public object[] GetParamsFromCommandString(SMCommand command, string cmdString)
-		{
-			// Create a new generic object list to hold the commands parameters
-			List<object> parameters = new List<object>();
-
-			// Handle passing the CommandName as the first arg if set in the SMCommand object
-			if (command.PassCommandAsFirstArg)
-			{
-				parameters.Add(command.CommandName);
-			}
-
-			// Handle passing a defined Form/QueryString param as the first arg if set in the SMCommand object
-			if (command.PassQueryParamAsFirstArg != null)
-			{
-				parameters.Add(Utils.GetQueryParam(command.PassQueryParamAsFirstArg));
-			}
-
-			// Get the Regex form the Command object to extract params from the user command string
-			string extractPattern = command.ParseExpression();
-
-			// Get the Match from runnong the extraction Regex
-			Match match = Regex.Match(cmdString, extractPattern);
-
-			// Check we have a match and process the matches capturing groups
-			if (match.Success)
-			{
-				foreach (Group group in match.Groups)
-				{
-					// Onlt process groups that aren't empty or the full command string
-					// the first matching group is always the full command string
-					if (group.Value != cmdString && group.Value != "")
-					{
-						parameters.Add(group.Value);
-					}
-				}
-			}
-
-			return parameters.ToArray();
-		}
-
-		/// <summary>
-		/// Runs the parsed command.
-		/// </summary>
-		/// <param name="command">Parsed Command.</param>
-		public void RunAction(SMParsedCommand command)
-		{
-			// Get the Class name of the command class for use in the ClassBuilder
-			string commandClassName = command.Command.CommandClass.Split('.').Last();
-
-			// Get class instance from ClassBuilder to run the command with
-			ClassBuilder cb = new ClassBuilder(commandClassName, this.UserID);
-
-			object commandClass = cb.GetClassInstance();
-
-			// if null returned by ClassBuilder
-			if (commandClass == null)
-			{
-				Utils.CallUserFuncArray(
-					command.Command.CommandClass,
-					command.Command.CommandMethod,
-					command.Parameters
-				);
-			}
-			// Using instance returned by the ClassBuilder
-			else
-			{
-				Utils.CallUserFuncArray(
-					commandClass,
-					command.Command.CommandMethod,
-					command.Parameters
-				);
-			}
 		}
 	}
 
---
+++ SlackMUDRPG/CommandClasses/SMCommandUtility.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SlackMUDRPG.Utility;
+using System.Threading;
+
+namespace SlackMUDRPG.CommandClasses
+{
+	public class SMCommandUtility
+	{
+		/// <summary>
+		/// Stores the characters UserID.
+		/// </summary>
+		private string UserID { get; set; }
+
+		/// <summary>
+		/// Stores an instance of SMCommandHelper to process the command.
+		/// </summary>
+		private SMCommandHelper CmdHelper;
+
+		/// <summary>
+		/// Class constructor.
+		/// </summary>
+		/// <param name="userID"></param>
+		public SMCommandUtility(string userID)
+		{
+			this.UserID = userID;
+		}
+
+		/// <summary>
+		/// Handles initialisation of the users command on a new thread if the command is valid.
+		/// </summary>
+		/// <param name="commandText"></param>
+		/// <returns>Empyt string if the command was passed off to a new thread, otherwise an error meesage.</returns>
+		public string InitateCommand(string commandText)
+		{
+			// Instantiate a new command helper.
+			this.CmdHelper = new SMCommandHelper(commandText);
+
+			// Validate command exists and the character has the appropriate access level to execute it before threading.
+			if (this.CmdHelper.CommandExists() && this.CmdHelper.CharacterCanExecuteCommand())
+			{
+				HttpContext ctx = HttpContext.Current;
+
+				Thread commandThread = new Thread(new ThreadStart(() =>
+				{
+					HttpContext.Current = ctx;
+					this.RunCommand(this.CmdHelper.GetParsedCommand());
+				}));
+
+				commandThread.Start();
+
+				return String.Empty;
+			}
+
+			// Return a message if the command does not exist or insufficient access level to execute.
+			return $"Command \"{Utils.SanitiseString(commandText)}\" not recognised, please check and try again";
+		}
+
+		/// <summary>
+		/// Runs the parsed command.
+		/// </summary>
+		/// <param name="command">Parsed Command.</param>
+		public void RunCommand(SMParsedCommand command)
+		{
+			// Get the Class name of the command class for use in the ClassBuilder
+			string commandClassName = command.Command.CommandClass.Split('.').Last();
+
+			// Get class instance from ClassBuilder to run the command with
+			ClassBuilder cb = new ClassBuilder(commandClassName, this.UserID);
+			object commandClass = cb.GetClassInstance();
+
+			// If null returned by ClassBuilder, run the command with the CommandClass name
+			// this lets UserCallFuncArray handle object instantiation.
+			if (commandClass == null)
+			{
+				Utils.CallUserFuncArray(
+					command.Command.CommandClass,
+					command.Command.CommandMethod,
+					command.Parameters
+				);
+			}
+			// Rm the command using the object instance returned by the ClassBuilder.
+			else
+			{
+				Utils.CallUserFuncArray(
+					commandClass,
+					command.Command.CommandMethod,
+					command.Parameters
+				);
+			}
+		}
+	}
+}
