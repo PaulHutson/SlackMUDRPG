a5f792f174803df3dcfbd2f3203b5cfaaf639762
Paul <Paul@PAUL-GAMING-PC>
2017-02-11 22:11:59 +0000

    Quest system basis.

---
+++ SlackMUDRPG/CommandClasses/SMQuest.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlackMUDRPG.CommandClasses
+{
+	public class SMQuest
+	{
+		[JsonProperty("QuestName")]
+		public string QuestName { get; set; }
+
+		[JsonProperty("Prerequisites")]
+		public string Prerequisites { get; set; }
+
+		[JsonProperty("QuestName")]
+		public List<SMQuestStep> QuestSteps { get; set; }
+	}
+
+	public class SMQuestPrequisite
+	{
+		[JsonProperty("Prerequisites")]
+		public string Type { get; set; } // Quest, Faction, Etc
+
+		[JsonProperty("Datapoint")]
+		public string Datapoint { get; set; } // Questname, Attribute Name, etc
+
+		[JsonProperty("Value")]
+		public string Value { get; set; } // The amount (1 for a quest completed, x for the attribute value)
+	}
+
+	public class SMQuestStep
+	{
+
+	}
+
+	public class SMQuestStatus
+	{
+		[JsonProperty("QuestName")]
+		public string QuestName { get; set; }
+
+		[JsonProperty("QuestStep")]
+		public string QuestStep { get; set; }
+
+		[JsonProperty("LastDateUpdated")]
+		public int LastDateUpdated { get; set; }
+
+		[JsonProperty("Completed")]
+		public bool Completed { get; set; }
+	}
+}
